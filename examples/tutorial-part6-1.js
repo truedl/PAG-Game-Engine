0f0e9f66fd4f08b3bbdd17d6ae6095a16a9197a7
Tomer <25082829+truedl@users.noreply.github.com>
2018-09-14 20:01:02 +0300

    6.1

---
+++ examples/tutorial-part6-1.js
@@ -0,0 +1,18 @@
+/*
+Part 6.1: Platformer Gravity
+
+First Platformer tutorial on gravity
+*/
+
+var pag = Game('tutorial', {'w': '500', 'h': '500'})
+// 1. Player Controller is required
+var player = new Controller(50, 10);
+
+// 2. Setup Platformer Gravity
+pag.platformer.gravity(24, 25, 440, player); // pag.platformer.gravity(runPerFrames, maxFallSpeed, fallToY, ControllerVariable)
+
+pag.update(24, function(){
+  // 3. Update Gravity
+  pag.platformer.gravityCheck(player); // pag.platformer.gravityCheck(ControllerVariable)
+  pag.draw.square(player.x, player.y, 40); // draw the player
+})
